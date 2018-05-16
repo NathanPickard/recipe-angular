863f97672a67fea1623dccf1fbb57d04af372093
Nathan Pickard <nathan.pickard87@gmail.com>
2018-05-15 22:09:03 -0700

    Adding data-storage service with put request to firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +13,15 @@ export class HeaderComponent {
   // onSelect(feature: string) {
   //   this.featureSelected.emit(feature);
   // }
+
+  constructor(private dataStorageService: DataStorageService) { }
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
 }
