f46c865f22571a6f5bc27448c7072b26b1987f5c
Nathan Pickard <nathan.pickard87@gmail.com>
2018-05-25 09:37:51 -0700

    Adding the request configuration and response

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -14,15 +14,19 @@ export class DataStorageService {
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.httpClient.put('https://ng-recipe-book-1346d.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+    return this.httpClient.put('https://ng-recipe-book-1346d.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes(), {
+      observe: 'body'
+    });
   }
 
   getRecipes() {
     const token = this.authService.getToken();
 
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-1346d.firebaseio.com/recipes.json?auth=' + token)
+    // this.httpClient.get<Recipe[]>('https://ng-recipe-book-1346d.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-1346d.firebaseio.com/recipes.json?auth=' + token, { observe: 'body', responseType: 'json' })
       .map(
         (recipes) => {
+          console.log(recipes)
           for (let recipe of recipes) {
             if (!recipe['ingredients']) {
               console.log(recipe);
