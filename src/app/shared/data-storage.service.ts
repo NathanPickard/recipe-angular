315538f61967500cd4dc55cc24d8e2acffc16be6
Nathan Pickard <nathan.pickard87@gmail.com>
2018-05-25 21:37:08 -0700

    Implementing Http parameters and progress via HttpClient

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 import 'rxjs/Rx';
 
 import { RecipeService } from '../recipes/recipe.service';
@@ -13,10 +13,16 @@ export class DataStorageService {
 
   storeRecipes() {
     const token = this.authService.getToken();
+    // const headers = new HttpHeaders().set('Authorization', 'Bearer bearerName');
 
-    return this.httpClient.put('https://ng-recipe-book-1346d.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes(), {
-      observe: 'body'
-    });
+    // return this.httpClient.put('https://ng-recipe-book-1346d.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //   params: new HttpParams().set('auth', token)
+    //   // headers: headers
+    // });
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-1346d.firebaseio.com/recipes.json', this.recipeService.getRecipes(), { reportProgress: true, params: new HttpParams().set('auth', token) })
+
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
