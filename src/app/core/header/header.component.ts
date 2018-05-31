0f4b9f80f20b336ea7192ee9581b9f20302bd39a
Nathan Pickard <nathan.pickard87@gmail.com>
2018-05-31 09:21:42 -0700

    Storing authentication state

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,21 +1,30 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 // import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 
 import { DataStorageService } from '../../shared/data-storage.service';
 import { AuthService } from '../../auth/auth.service';
+import * as fromApp from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  authState: Observable<fromAuth.State>
   // @Output() featureSelected = new EventEmitter<string>();
 
   // onSelect(feature: string) {
   //   this.featureSelected.emit(feature);
   // }
 
-  constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService, private store: Store<fromApp.AppState>) { }
+
+  ngOnInit() {
+    this.authState = this.store.select('auth');
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
