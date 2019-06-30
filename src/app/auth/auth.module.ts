0b596a3b15945a9f3e1538513cf09d78edf1a898
Nathan Pickard <nathan.pickard87@gmail.com>
2019-06-30 13:50:08 -0700

    Adding error handling on signup and implemented login requests

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
 import { AuthComponent } from "./auth.component";
 import { SigninComponent } from "./signin/signin.component";
 import { SignupComponent } from "./signup/signup.component";
 import { AuthRoutingModule } from "./auth-routing.module";
+import { LoadingSpinnerComponent } from "../shared/loading-spinner/loading-spinner.component";
 
 @NgModule({
   declarations: [
     SigninComponent,
     SignupComponent,
-    AuthComponent
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
   imports: [
+    CommonModule,
     FormsModule,
     AuthRoutingModule
   ]
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 // import { Store } from '@ngrx/store';
 
 // import * as fromApp from '../store/app.reducers';
 // import * as AuthActions from './store/auth.actions';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -31,51 +34,73 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
+    )
+      .pipe(catchError(errorRes => {
+        let errorMessage = 'An unknown error occurred!';
+        if (!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'This email exists already';
+        }
+        return throwError(errorMessage);
+      }));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBqi90f9DrkzzpaXyCTtUxkoVTurQmMyPo',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
     );
   }
 
-  signupUser(email: string, password: string) {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(
-        user => {
-          // this.store.dispatch(new AuthActions.Signup());
-          firebase.auth().currentUser.getIdToken()
-            .then(
-              (token: string) => {
-                // this.store.dispatch(new AuthActions.SetToken(token));
-              }
-            )
-        }
-      )
-      .catch(
-        error => console.log(error)
-      )
-  }
+  // signupUser(email: string, password: string) {
+  //   firebase.auth().createUserWithEmailAndPassword(email, password)
+  //     .then(
+  //       user => {
+  //         // this.store.dispatch(new AuthActions.Signup());
+  //         firebase.auth().currentUser.getIdToken()
+  //           .then(
+  //             (token: string) => {
+  //               // this.store.dispatch(new AuthActions.SetToken(token));
+  //             }
+  //           )
+  //       }
+  //     )
+  //     .catch(
+  //       error => console.log(error)
+  //     )
+  // }
 
-  signinUser(email: string, password: string) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(
-        response => {
-          // this.store.dispatch(new AuthActions.Signin());
-          this.router.navigate(['/']);
-          firebase.auth().currentUser.getIdToken()
-            .then(
-              (token: string) => {
-                // this.store.dispatch(new AuthActions.SetToken(token));
-              }
-            )
-        }
-      )
-      .catch(
-        error => console.log(error)
-      );
-  }
+  // signinUser(email: string, password: string) {
+  //   firebase.auth().signInWithEmailAndPassword(email, password)
+  //     .then(
+  //       response => {
+  //         // this.store.dispatch(new AuthActions.Signin());
+  //         this.router.navigate(['/']);
+  //         firebase.auth().currentUser.getIdToken()
+  //           .then(
+  //             (token: string) => {
+  //               // this.store.dispatch(new AuthActions.SetToken(token));
+  //             }
+  //           )
+  //       }
+  //     )
+  //     .catch(
+  //       error => console.log(error)
+  //     );
+  // }
 
-  logout() {
-    firebase.auth().signOut();
-    // this.store.dispatch(new AuthActions.Logout());
-    // this.token = null;
-  }
+  // logout() {
+  //   firebase.auth().signOut();
+  //   // this.store.dispatch(new AuthActions.Logout());
+  //   // this.token = null;
+  // }
 
   // getToken() {
   //   firebase.auth().currentUser.getIdToken()
