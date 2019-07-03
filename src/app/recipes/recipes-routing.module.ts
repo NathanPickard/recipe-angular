0bd022e9129f266e87ca677ad343d456182da9c9
Nathan Pickard <nathan.pickard87@gmail.com>
2019-07-02 17:50:28 -0700

    Adding Auth guard service to protect routes

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -5,16 +5,19 @@ import { RecipesComponent } from "./recipes.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
-import { AuthGuard } from "../auth/auth-guard.service";
+// import { AuthGuard } from "../auth/auth-guard.service";
+import { AuthGuard } from "../auth/auth.guard";
 import { RecipesResolverService } from "./recipes-resolver.service";
 
 const recipesRoutes: Routes = [
   {
-    path: '', component: RecipesComponent, children: [
+    path: '', component: RecipesComponent,
+    canActivate: [AuthGuard],
+    children: [
       { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
+      { path: 'new', component: RecipeEditComponent, /* canActivate: [AuthGuard] */ },
       { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService], canActivate: [AuthGuard] }
+      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService], /* canActivate: [AuthGuard] */ }
     ]
   },
 ];
