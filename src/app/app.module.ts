d9a7bee391f2275d4cb2446df68116377e7926cf
Nathan Pickard <nathan.pickard87@gmail.com>
2019-07-03 22:06:15 -0700

    Recipes feature module refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
@@ -25,11 +26,12 @@ import { AuthEffects } from './auth/store/auth.effects';
     AppComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: "my-app"}),
+    BrowserModule.withServerTransition({ appId: "my-app" }),
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
     ShoppingListModule,
+    RecipesModule,
     AuthModule,
     CoreModule,
     StoreModule.forRoot(reducers),
